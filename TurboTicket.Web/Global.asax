50d909b7e9e33f6809af66c8a1f91071daf982a6
CodyJohnston <teamrocketops@gmail.com>
2016-08-09 01:16:13 -0700

    Add project files.

---
+++ TurboTicket.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TurboTicket.Web.MvcApplication" Language="C#" %>
---
+++ TurboTicket.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TurboTicket.Web.Startup))]
+namespace TurboTicket.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
